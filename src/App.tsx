3d66d0b69e66996f5d3241a34ede85bbe463be01
unknown <souhail7ouabou@gmail.com>
2023-01-04 15:46:33 +0100

    Styles Prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Button } from './components/Button'
+import { Container } from './components/Container'
 import { Greet } from './components/Greet'
 import { Heading } from './components/Heading'
 import { Input } from './components/input'
@@ -41,10 +42,13 @@ function App() {
             <Greet name="Shlo" isLoggedIn={false} />
             <Button
                 handleClick={(event, id) => {
-                    console.log('Button Clicked', event,id)
+                    console.log('Button Clicked', event, id)
                 }}
             />
-            <Input value='' handleChange={(event)=>console.log(event)} />
+            <Input value="" handleChange={(event) => console.log(event)} />
+            <Container
+                styles={{ border: '1px solid black', padding: '1rem' }}
+            />
         </div>
     )
 }
